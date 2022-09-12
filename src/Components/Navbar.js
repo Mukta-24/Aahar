05541b36a9b2ef3004782f82984a0dc72384dd59
Mukta <mukta.gupta08@gmail.com>
2022-09-12 15:42:52 +0530

    navbar completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { Navbar, Nav } from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,19 +7,28 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar() {
     return (
-        <div className='mt-2'>
-            <Navbar bg="light" variant="light">
+        <div>
+            <Navbar className='mt-1' bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home" className='heading'>
-                    <span className='icon'><img src='box.png'/></span> AAHAR</Navbar.Brand>
+                    <Navbar.Brand href="#home" style={{padding:0, margin:0}} >
+                        <div className='title'>
+                            <div className='icon'>
+                                <img src='aahar_logo-removebg-preview.png' />
+                            </div>
+                            <div className='heading'>AAHAR
+                            </div>
+                        </div>
+                    </Navbar.Brand>
                     <div className='align-text'>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#add">About Us</Nav.Link>
-                        <Nav.Link href="#menu">Menu</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                        <Nav.Link href="#signup">Sign Up</Nav.Link>
-                    </Nav>
+                        <Nav className="me-auto navigation">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#add">About Us</Nav.Link>
+                            <Nav.Link href="#menu">Menu</Nav.Link>
+                            <Nav.Link href="#login">Login</Nav.Link>
+                            <Nav.Link href="#signup">Sign Up</Nav.Link>
+                            <Nav.Link href="#cart"><i className="fa-sharp fa-solid fa-cart-shopping"></i></Nav.Link>
+                            
+                        </Nav>
                     </div>
                 </Container>
             </Navbar>
