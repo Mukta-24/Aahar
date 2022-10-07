b700db726d1ba73cdd9aa54b6f165947e8f5e2b8
Mukta <mukta.gupta08@gmail.com>
2022-10-07 23:36:19 +0530

    new feature added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar';
 import MainHeading from './Components/MainHeading';
+import NewF from './Components/NewF';
+import Special_dishes from './Components/Special_dishes';
 
 function App() {
   return (
     <>
     <NavBar/>
     <MainHeading/>
+    <NewF/>
+    <Special_dishes/>
+    
     </>
   );
 }
